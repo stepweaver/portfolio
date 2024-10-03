41a6dda9bffa437c93e98fc1effc233a3ef33f5f
stepweaver <stephen@stepweaver.dev>
2024-10-03 07:59:36 -0400

    Remove react router dom

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { ThemeProvider } from './common/ThemeContext.jsx';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router basename='/portfolio'>
       <ThemeProvider>
         <App />
       </ThemeProvider>
-    </Router>
   </React.StrictMode>
 );
