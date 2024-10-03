2e97404d0c832b98b228e5f7f909807754c3c17e
stepweaver <stephen@stepweaver.dev>
2024-10-02 21:35:24 -0400

    Install react-router-dom and include basename in main

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { ThemeProvider } from './common/ThemeContext.jsx';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <Router basename='/portfolio'>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </Router>
   </React.StrictMode>
 );
